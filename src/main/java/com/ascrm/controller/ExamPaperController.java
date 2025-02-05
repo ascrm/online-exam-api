f396f428b49e629ad85f06235d9daba6be3f6bd1
ascrm <2339621373@qq.com>
2025-02-05 21:55:22 +0800

    查询用户考试列表功能

--- src/main/java/com/ascrm/controller/ExamPaperController.java
+++ src/main/java/com/ascrm/controller/ExamPaperController.java
@@ -2,9 +2,12 @@ package com.ascrm.controller;
 
 import com.ascrm.converter.ExamPaperConverter;
 import com.ascrm.entity.ExamPaper;
+import com.ascrm.entity.HistoryExam;
 import com.ascrm.entity.PageResult;
 import com.ascrm.entity.Result;
 import com.ascrm.service.ExamPaperService;
+import com.ascrm.service.HistoryExamService;
+import com.ascrm.utils.UserHolder;
 import com.ascrm.viewer.ExamPaperViewer;
 import com.mybatisflex.core.paginate.Page;
 import com.mybatisflex.core.query.QueryWrapper;
@@ -16,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.ascrm.entity.table.ExamPaperTableDef.EXAM_PAPER;
+import static com.ascrm.entity.table.HistoryExamTableDef.HISTORY_EXAM;
 
 /**
  * 控制层。
@@ -31,6 +35,8 @@ public class ExamPaperController {
     private final ExamPaperService examPaperService;
 
     private final ExamPaperConverter examPaperConverter;
+
+    private final HistoryExamService historyExamService;
 
     /**
      * 分页查询试卷列表
@@ -103,6 +109,27 @@ public class ExamPaperController {
                 .where(EXAM_PAPER.IS_DELETE.eq(0))
                 .and(EXAM_PAPER.NAME.eq(examPaper.getName()))
                 .and(EXAM_PAPER.IS_PUBLISHED.eq(examPaper.getIsPublished()))
+                .list();
+        return Result.success(examPaperConverter.to(examPaperList));
+    }
+
+    /**
+     * 查询用户考试列表
+     */
+    @GetMapping("/examPaper/userExamList")
+    public Result<List<ExamPaperViewer>> getUserExamList(){
+        List<HistoryExam> historyExamList = historyExamService.queryChain().select(HISTORY_EXAM.ALL_COLUMNS)
+                .from(HISTORY_EXAM)
+                .where(HISTORY_EXAM.USERNAME.eq(UserHolder.getUsername()))
+                .and(HISTORY_EXAM.IS_DELETE.eq(0))
+                .list();
+        List<Integer> ids = historyExamList.stream().map(HistoryExam::getExamPaperId).toList();
+        List<ExamPaper> examPaperList = examPaperService.queryChain()
+                .select(EXAM_PAPER.ALL_COLUMNS)
+                .from(EXAM_PAPER)
+                .where(EXAM_PAPER.IS_DELETE.eq(0))
+                .and(EXAM_PAPER.IS_PUBLISHED.eq(1))
+                .and(EXAM_PAPER.ID.notIn(ids))
                 .list();
         return Result.success(examPaperConverter.to(examPaperList));
     }
